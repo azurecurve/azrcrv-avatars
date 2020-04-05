5c0f786320fb69867129bac0a670eb8be9dbfaed
azurecurve <github@azurecurve.co.uk>
2020-04-05 13:12:43 +0100

    * Fix bug with setting of default options.  * Fix bug with plugin menu.  * Update plugin menu css.

--- azrcrv-avatars.php
+++ azrcrv-avatars.php
@@ -3,7 +3,7 @@
  * ------------------------------------------------------------------------------
  * Plugin Name: Avatars
  * Description: Allow users to upload their own avatar.
- * Version: 1.1.3
+ * Version: 1.1.4
  * Author: azurecurve
  * Author URI: https://development.azurecurve.co.uk/classicpress-plugins/
  * Plugin URI: https://development.azurecurve.co.uk/classicpress-plugins/avatars/
@@ -24,7 +24,7 @@ if (!defined('ABSPATH')){
 
 // include plugin menu
 require_once(dirname( __FILE__).'/pluginmenu/menu.php');
-register_activation_hook(__FILE__, 'azrcrv_create_plugin_menu_a');
+add_action('admin_init', 'azrcrv_create_plugin_menu_a');
 
 // include update client
 require_once(dirname(__FILE__).'/libraries/updateclient/UpdateClient.class.php');
@@ -36,7 +36,7 @@ require_once(dirname(__FILE__).'/libraries/updateclient/UpdateClient.class.php')
  *
  */
 // add actions
-register_activation_hook(__FILE__, 'azrcrv_a_set_default_options');
+add_action('admin_init', 'azrcrv_a_set_default_options');
 
 // add actions
 add_action('admin_menu', 'azrcrv_a_create_admin_menu');
@@ -120,6 +120,7 @@ function azrcrv_a_set_default_options($networkwide){
 	
 	$new_options = array(
 						'localonly' => 0,
+						'updated' => strtotime('2020-04-04'),
 			);
 	
 	// set defaults for multi-site
@@ -162,17 +163,24 @@ function azrcrv_a_update_options($option_name, $new_options, $is_network_site){
 		if (get_site_option($option_name) === false){
 			add_site_option($option_name, $new_options);
 		}else{
-			update_site_option($option_name, azrcrv_a_update_default_options($new_options, get_site_option($option_name)));
+			$options = get_site_option($option_name);
+			if (!isset($options['updated']) OR $options['updated'] < $new_options['updated'] ){
+				$options['updated'] = $new_options['updated'];
+				update_site_option($option_name, azrcrv_a_update_default_options($options, $new_options));
+			}
 		}
 	}else{
 		if (get_option($option_name) === false){
 			add_option($option_name, $new_options);
 		}else{
-			update_option($option_name, azrcrv_a_update_default_options($new_options, get_option($option_name)));
+			$options = get_option($option_name);
+			if (!isset($options['updated']) OR $options['updated'] < $new_options['updated'] ){
+				$options['updated'] = $new_options['updated'];
+				update_option($option_name, azrcrv_a_update_default_options($options, $new_options));
+			}
 		}
 	}
 }
-
 
 /**
  * Add default options to existing options.
@@ -185,10 +193,10 @@ function azrcrv_a_update_default_options( &$default_options, $current_options ) 
     $current_options = (array) $current_options;
     $updated_options = $current_options;
     foreach ($default_options as $key => &$value) {
-        if (is_array( $value) && isset( $updated_options[$key ])){
-            $updated_options[$key] = azrcrv_a_update_default_options($value, $updated_options[$key], true);
+        if (is_array( $value) && isset( $updated_options[$key])){
+            $updated_options[$key] = azrcrv_a_update_default_options($value, $updated_options[$key]);
         } else {
-            $updated_options[$key] = $value;
+			$updated_options[$key] = $value;
         }
     }
     return $updated_options;
