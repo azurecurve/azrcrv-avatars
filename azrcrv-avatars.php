8e691303bde2d0793552d7101f3436d27cdb10ed
azurecurve <github@azurecurve.co.uk>
2025-05-31 11:16:54 +0100

    Release v2.2.5

--- azrcrv-avatars.php
+++ azrcrv-avatars.php
@@ -3,8 +3,9 @@
  * ------------------------------------------------------------------------------
  * Plugin Name:		Avatars
  * Description:		Allow users to upload their own avatar.
- * Version:			2.2.4
+ * Version:			2.2.5
  * Requires CP:		1.0
+ * Requires PHP:	7.4
  * Author:			azurecurve
  * Author URI:		https://development.azurecurve.co.uk/classicpress-plugins/
  * Plugin URI:		https://development.azurecurve.co.uk/classicpress-plugins/avatars/
